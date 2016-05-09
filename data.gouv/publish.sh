4859a87990fd81740f54d1d602eb03c802114407
Lexman <tuttle@lexman.org>
2016-05-09 19:34:43 +0200

    publication avec le nom du département

--- data.gouv/publish.sh
+++ data.gouv/publish.sh
@@ -4,11 +4,12 @@ export XAPIKEY="eyJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiNTRiYWViNGJjNzUxZGY2MmI1NWZhNWE
 DEPTS_CONT=`cat ../departement_metropolitain.txt|tr '\r\n' '  '`
 DEPTS_LOIN="2A 2B 971 972 973 974 976"
 
-#http POST $API'datasets/carte-des-departements-2/resources/' X-API-KEY:$XAPIKEY title="departement-$NUM" filesize=$FILESIZE format="shapefile" mime="application/zip" url="http://sisyphus.lexman.org/workflows/carte_de_mon_departement/workspace/publish/departement-$NUM.zip" 
 
+echo "$DEPTS_CONT $DEPTS_LOIN"
 for NUM in $DEPTS_CONT $DEPTS_LOIN
 do
-    echo $NUM
-    #$FILESIZE=`du ../zips/departement-$NUM.zip | awk '{print $1}'`
-    http POST $API'datasets/carte-des-departements-2/resources/' X-API-KEY:$XAPIKEY title="departement-$NUM" format="shapefile" mime="application/zip" url="http://sisyphus.lexman.org/workflows/carte_de_mon_departement/workspace/publish/departement-$NUM.zip" 
+    NOM=`grep -r "^$NUM" ../noms_departements.txt | awk '{print $2}'`
+    echo $NUM - $NOM
+    #$FILESIZE=`du ../zips/departement-$NUM-$NOM.zip | awk '{print $1}'`
+    http POST $API'datasets/carte-des-departements-2/resources/' X-API-KEY:$XAPIKEY title="departement-$NUM-$NOM" format="shapefile" mime="application/zip" url="http://sisyphus.lexman.org/workflows/carte_de_mon_departement/workspace/publish/departement-$NUM-$NOM.zip" 
 done
