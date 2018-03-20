76703e3e5985a42de84ad8b3f6ffa5de5052ec4d
lexman <android@lexman.org>
2018-03-20 07:13:04 +0100

    Meilleure prise en compte des départements fractionnés (par exemple avec des iles) afin de corriger le problème des Pyrénnées Atlantiques remontées sur datagouv

--- shp2poly_departements.py
+++ shp2poly_departements.py
@@ -15,11 +15,16 @@ def shp2polys(shp_file, dest_dir):
         dept_code = records[counter][0]
         output_name = "departement-{}.poly".format(dept_code)
         with open (os.path.join(dest_dir, output_name), 'w') as target:
-            target.write("Departement {}\n".format(dept_code))		
-            target.write("1\n".format(dept_code))		
-            for coords in shapeobject.points:
-                coords_str = "    {}    {}".format(coords[0], coords[1]).replace(",", ".")
-                target.write("{}\n".format(coords_str))		
-            target.write("END\nEND\n".format(coords_str))		
+            target.write("Departement {}\n".format(dept_code))      
+            start_idx = 0
+            end_idxs = [p for p in shapeobject.parts][1:] + [len(shapeobject.points)]
+            poly_num = 1
+            for end_idx in end_idxs:
+                target.write("{}\n".format(poly_num))
+                for coords in shapeobject.points[start_idx:end_idx]:
+                    coords_str = "    {}    {}".format(coords[0], coords[1]).replace(",", ".")
+                    target.write("{}\n".format(coords_str))
+                target.write("END\n")
+                start_idx = end_idx    
+            target.write("END\n")
         counter = counter + 1
-
